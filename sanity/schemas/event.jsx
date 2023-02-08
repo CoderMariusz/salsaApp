5ea5676fa7255f881ce2732b00846a0fe88249e8
Mariusz Krawczyk <przyslony@gmail.com>
2023-02-08 21:42:23 +0000

    add event to sanity

---
+++ sanity/schemas/event.jsx
@@ -0,0 +1,41 @@
+import { defineField, defineType } from 'sanity';
+
+export default defineType({
+  name: 'event',
+  title: 'Event',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string'
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 96
+      }
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'string'
+    }),
+    defineField({
+      name: 'price',
+      title: 'Price',
+      type: 'number'
+    }),
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true
+      }
+    })
+  ]
+});
